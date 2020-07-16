d9f7e59903c4ded5f813fd0c51c56c20bb25276f
Kredek74 <Krdek74@protonmail.com>
2020-07-17 00:40:13 +0200

    Kodilla-Task-6.1

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,37 +1,7 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.user.SimpleUser;
-import com.kodilla.testing.calculator.Calculator;
-
 public class TestingMain {
     public static void main(String[] args){
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        // tu umieść kolejny test ->
-        System.out.println("Test - pierwszy test jednostkowy:");
-
-        Calculator calculator = new Calculator();
-
-        int addResult = 12;
-        int subtractResult = 8;
-
-        if (addResult == calculator.additionAtoB(7, 5)){
-            System.out.println("Addition works");
-        } else {
-            System.out.println("Error of method add");
-        }
-        if (subtractResult == calculator.substractionAfromB(14, 6)) {
-            System.out.println("Substraction works");
-        } else {
-            System.out.println("Error of method subtract");
-        }
+        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
     }
 }
